9be79a88b77335f35f02633cb4980d5a0b5a8797
Aakash Singh <113110858+aakash894@users.noreply.github.com>
2023-06-02 14:06:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+/*
+   EC2 Template
+*/
+
+// Module Template
+module "ec2_instance" {
+  source  = "nvwlminfhcts001.dryicelabs.com/podtfe/ec2/aws"
+  version = "1.0.0"
+  name = "tfe-ec2-instance"
+  public_ip = true
+  tags = {
+    "Environment" = "Dev"
+    "Provisioned" = "TFE"
+  }
+  subnet = "subnet-02d3f044075bbe8bb"
+  volume_size = 20
+  ami_id = "ami-0cce6b9dac6aadc8e"
+  key_name = "mycloud"
+  instance_type = "t2.micro"
+  security_groups = ["sg-0c202a5babc888c56"]
+}
+
+// Template Outputs
+
+// output "instance_id" {
+//   value = module.ec2_instance.id
+// }
+
+// output "ip_address" {
+//   value = module.ec2_instance.private_ip
+// }
