1be18b0df5165bfb12ec84ea29def746f315ca52
Aakash Singh <113110858+aakash894@users.noreply.github.com>
2023-06-02 14:18:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -15,7 +15,13 @@ terraform {
 
 variable "region" {}
 
+variable "access_key" {}
+
+variable "secret_key" {}
+
 provider "aws" {
   # Configuration options
   region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
